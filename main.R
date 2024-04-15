6342eedd2d55c5180fe7b93d5cc287257a0b5c86
saifshehata <saif.shehata@gmail.com>
2024-04-15 21:40:42 +0100

    Shorten names

--- main.R
+++ main.R
@@ -1,10 +1,10 @@
 library(tidyverse)
 library(here)
 
-tv_hours_table <- gss_cat %>% 
+tv_hours_tab <- gss_cat %>% 
   filter(age < 30) %>% 
   group_by(marital) %>% 
   summarise((mean_tv_hours = mean(tvhours, na.rm = TRUE)))
 
 
-write_csv(tv_hours_table, here("TV_Hours_by_Marital.csv"))
+write_csv(tv_hours_tab, here("TV_Hours_by_Marital.csv"))
